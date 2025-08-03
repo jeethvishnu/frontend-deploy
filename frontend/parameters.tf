1d7a55927548ab7b9915925e3ff7ed1659f55fa3
vishnu jeeth <jeeth.pam99@gmail.com>
2025-08-03 21:55:49 +0530

    added

--- frontend/parameters.tf
+++ frontend/parameters.tf
@@ -1,4 +1,4 @@
-resource "aws_ssm_parameter" "aws_lb_listener_arn" {
+resource "aws_ssm_parameter" "aws_alb_listener_arn" {
     name = "/${var.project}/${var.env}/alb_listener_arn"
     type = "String"
     value = aws_lb_listener.frontend.arn
